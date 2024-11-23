51facb406a0bab86d92e557628618f1474c07e07
JuanCarlos <jcreyes@ferochi.cl>
2024-11-22 23:51:50 -0300

    primer commit chao

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que el formulario se envíe por defecto
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Validar usuario y contraseña
+    if (username === "admin" && password === "admin1234") {
+        // Redirige a la página bienvenido.html
+        window.location.href = "bienvenido.html";
+    } else {
+        document.getElementById("error-message").textContent = "Usuario o contraseña incorrectos";
+    }
+});
